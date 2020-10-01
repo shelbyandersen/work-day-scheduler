3bf9a5b87c9bcfb2567b64c419ee39fd9f496e30
Shelby <shelbyrobison14@gmail.com>
2020-10-01 12:02:12 -0400

    Javascript is pseudocoded

---
+++ script.js
@@ -0,0 +1,16 @@
+//DOM VARIABLES
+//JS VARIABLES
+//FUNCTION DEFINITIONS
+//FUNCTION CALLS
+
+//Current day is displayed in heading (momentJS)
+
+//Create time blocks for 9AM - 5PM
+
+//Grey - Time has passed, Red - Current hour, Green - Open
+
+//User can type in the time blocks when they click in it
+
+//Lock button saves the event (saved in local storage)
+
+//When page is refreshed events should still show
