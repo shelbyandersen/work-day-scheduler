c1c88f738a0ace78e886e16dbd11063dc788c28a
Shelby <shelbyrobison14@gmail.com>
2020-10-03 14:22:50 -0400

    Added current day to JS

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 //DOM VARIABLES
+var workDay = [
+  "8AM",
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+];
+
+var currentDay = moment().format("LLL");
+console.log(currentDay);
+
 //JS VARIABLES
 //FUNCTION DEFINITIONS
 //FUNCTION CALLS
 
 //Current day is displayed in heading (momentJS)
+$("#currentDay").append(currentDay);
 
 //Create time blocks for 9AM - 5PM
 
