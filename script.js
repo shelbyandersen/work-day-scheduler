cb7e45f46bbafe38963b42a71ef5203a916497bd
Shelby <shelbyrobison14@gmail.com>
2020-10-06 16:24:13 -0400

    time block elements are working and they update colors for past, present, and future time

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-//DOM VARIABLES
-var workDay = [
-  "8AM",
-  "9AM",
-  "10AM",
-  "11AM",
-  "12PM",
-  "1PM",
-  "2PM",
-  "3PM",
-  "4PM",
-  "5PM",
-];
+$(document).ready(function () {
+  // DOM VARIABLES
+  var currentDay = moment().format("dddd, MMMM Do");
+  var nineAm = $("09");
+  var tenAm = $("10");
+  var elevenAm = $("11");
+  var noon = $("12");
+  var onePm = $("13");
+  var twoPm = $("14");
+  var threePm = $("15");
+  var fourPm = $("16");
+  var fivePm = $("17");
+  var hour = moment().hours();
 
-var currentDay = moment().format("dddd, MMMM Do");
-console.log(currentDay);
+  // Current day is displayed in heading (momentJS)
+  $("#currentDay").append(currentDay);
 
-//JS VARIABLES
-//FUNCTION DEFINITIONS
-//FUNCTION CALLS
+  // Change textarea background color based on time
+  // Grey - Time has passed, Red - Current hour, Green - Future
 
-//Current day is displayed in heading (momentJS)
-$("#currentDay").append(currentDay);
+  var checkTime = function () {
+    //Get current time
+    var currentTime = moment().format("H");
 
-//Create time blocks for 9AM - 5PM in JS
+    // Elements with class "taskarea" - these are the time-blocks
+    var timeBlockElements = $(".form-control");
 
-//Grey - Time has passed, Red - Current hour, Green - Open
+    // Loop through the taskarea elements to check time
+    for (var i = 0; i < timeBlockElements.length; i++) {
+      //Get element i's ID as a string
+      var elementID = timeBlockElements[i].id;
 
-//User can type in the time blocks when they click in it
+      // Get elements by ID
+      var timeEl = document.getElementById(timeBlockElements[i].id);
 
-//Lock button saves the event (saved in local storage)
+      // Remove old class from element
+      $(timeBlockElements[i].id).removeClass(".present .past .future");
 
-//When page is refreshed events should still show
+      // Add new past/present/future class based on current hour
+      if (elementID < currentTime) {
+        $(timeEl).addClass("past");
+      } else if (elementID > currentTime) {
+        $(timeEl).addClass("future");
+      } else {
+        $(timeEl).addClass("present");
+      }
+    }
+  };
+
+  // Check the time every 5 minutes
+  setInterval(checkTime(), 1000 * 60 * 5);
+
+  //Lock button saves the event (saved in local storage)
+});
